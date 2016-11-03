d0b3b95d9bdcdf11e820164640ae5fdd990ac8bf
Hideharu Suzuki <hideharu7777777@gmail.com>
2016-11-03 18:38:38 +0900

    プッシュ通知・ローカル通知のワークスペース・サーバープロジェクト・ローカルプロジェクト追加

---
+++ PushLocalNotificationClient/PushLocalNotificationClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushLocalNotificationClient
+//
+//  Created by Suzuki Hideharu on 2016/11/03.
+//  Copyright © 2016年 Suzuki.Works. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PushLocalNotificationServer/PushLocalNotificationServer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PushLocalNotificationServer
+//
+//  Created by Suzuki Hideharu on 2016/11/03.
+//  Copyright © 2016年 Suzuki.Works. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
